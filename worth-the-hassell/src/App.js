6fdad233559e9303e3c8c242764aa90bf7605fd7
Nicholas Hassell <hasselln@gmail.com>
2025-07-20 00:42:22 -0700

    working nav bar

--- worth-the-hassell/src/App.js
+++ worth-the-hassell/src/App.js
@@ -3,9 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About';
 import Contact from './pages/Contact';
+import DrawerAppBar from './components/MuiNavbar';
 
 function App() {
   return (
+    <>
+    <DrawerAppBar />
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -13,6 +16,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
+    </>
   );
 }
 
