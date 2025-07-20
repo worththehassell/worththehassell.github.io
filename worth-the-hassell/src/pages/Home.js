e94579204cac956c2ac38bec907fa6a21be673f1
Nicholas Hassell <hasselln@gmail.com>
2025-07-19 22:46:44 -0700

    add nav bar

--- worth-the-hassell/src/pages/Home.js
+++ worth-the-hassell/src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 function Home() {
   return (
     <div>
+      <Navbar></Navbar>
       <h1>Welcome to the Home Page!</h1>
     </div>
   );
